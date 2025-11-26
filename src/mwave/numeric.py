658127f001746cc7eeebf2bdae25c44ba69c6aa4
jc-roth <2797077+jc-roth@users.noreply.github.com>
2025-11-25 18:21:17 -0800

    Added documentation

--- src/mwave/numeric.py
+++ src/mwave/numeric.py
@@ -1,8 +1,20 @@
 import numpy as np
 
 class NumericBraggInterferometer:
+    """
+    A class for numerically simulating Bragg interferometers. This class allows for the definition of an interferometer geometry and the numerical propagation of wavefunctions through that geometry.
+    """
 
     def __init__(self, kmin, kmax, distance, kpad = 10, x0=0, t0=0, k0=0):
+        """
+        :param kmin: The minimum momentum state to include in the simulation grid.
+        :param kmax: The maximum momentum state to include in the simulation grid.
+        :param distance: The number of momentum states to keep on either side of the Bragg diffraction orders.
+        :param kpad: The padding to add to the momentum grid. Defaults to 10.
+        :param x0: The initial position. Defaults to 0.
+        :param t0: The initial time. Defaults to 0.
+        :param k0: The initial momentum. Defaults to 0.
+        """
 
         # Ensure kpad is even
         if kpad % 2 != 0:
@@ -22,6 +34,11 @@ class NumericBraggInterferometer:
         self.operations = []
 
     def split(self, klattice):
+        """
+        Applies a Bragg diffraction splitting operation to all nodes at the current level.
+
+        :param klattice: The lattice wavevector(s) for the Bragg pulse. Can be a single value or a list of values.
+        """
         # Store operation in list
         self.operations.append(('split', [klattice]))
 
@@ -33,6 +50,11 @@ class NumericBraggInterferometer:
         self.current_level = next_level
 
     def propagate(self, t):
+        """
+        Propagates the state of all nodes at the current level for a time :code:`t`.
+
+        :param t: The duration of the propagation.
+        """
         # Store operation in list
         self.operations.append(('propagate', [t]))
 
@@ -44,6 +66,11 @@ class NumericBraggInterferometer:
         self.current_level = next_level
 
     def get_nodes(self):
+        """
+        Returns a dictionary of nodes at the current level, organized by momentum and position.
+
+        :returns: A dictionary where keys are momentum states, values are dictionaries of positions, and values of those dictionaries are lists of nodes at that momentum and position.
+        """
         # Make a dictionary of momentum states. Values are dictionaries of positions. Values of those dictionaries are lists of nodes at that momentum and position.
         nodedict = {}
         for node in self.current_level:
@@ -56,10 +83,24 @@ class NumericBraggInterferometer:
         return nodedict
     
     def set_operation_funcs(self, funcs):
+        """
+        Sets the functions used to numerically evaluate the operations (split, propagate) performed on the interferometer.
+
+        :param funcs: A list of functions corresponding to the operations performed on the interferometer, in order.
+        """
         # Store the supplied function list in the NumericBraggInterferometer object
         self.operation_funcs = funcs
 
     def get_population_func(self, momentums, func_pop_init, func_wf_init, func_wf2_init):
+        """
+        Returns a function that computes the population for a given set of output momentums.
+
+        :param momentums: A list of output momentums to compute the population for.
+        :param func_pop_init: A function that computes the initial population.
+        :param func_wf_init: A function that computes the initial wavefunction.
+        :param func_wf2_init: A function that computes the initial wavefunction (secondary, used in calculation).
+        :returns: A function that takes :code:`comm_args` and returns the total population at the specified momentums.
+        """
         # Get dictionary of momentums
         nodedict = self.get_nodes()
 
@@ -107,6 +148,13 @@ class NumericTreeNode:
     """
 
     def __init__(self, interferometer, x, t, k, parent=None):
+        """
+        :param interferometer: The :code:`NumericBraggInterferometer` instance this node belongs to.
+        :param x: The position of the node.
+        :param t: The time of the node.
+        :param k: The momentum of the node.
+        :param parent: The parent node. Optional.
+        """
         self.ifr = interferometer
         self.x = x
         self.t = t
@@ -115,6 +163,13 @@ class NumericTreeNode:
         self.children = []
 
     def split(self, klattice, distance=4, filter_func=lambda kvec, k0, kf: True):
+        """
+        Splits the node based on Bragg diffraction, creating child nodes for the diffracted states.
+
+        :param klattice: The lattice wavevector(s).
+        :param distance: The number of momentum states to keep around the diffraction orders. Defaults to 4.
+        :param filter_func: A function to filter which child nodes are created. Defaults to allowing all.
+        """
 
         # Put klattice into a list if not already
         if not isinstance(klattice, (list, tuple, np.ndarray)):
@@ -142,10 +197,20 @@ class NumericTreeNode:
                 self.children.append(NumericTreeNode(self.ifr, x=self.x, t=self.t, k=k, parent=self))
 
     def propagate(self, T):
+        """
+        Propagates the node for a time :code:`T`, creating a single child node.
+
+        :param T: The duration of propagation.
+        """
         # Make a single child node with a position and time updated according to the propagation time
         self.children.append(NumericTreeNode(self.ifr, x=self.x + self.k*T, t=self.t + T, k=self.k, parent=self))
 
     def get_ancestry(self):
+        """
+        Returns a list of ancestor nodes from the root node to this node.
+
+        :returns: A list of :code:`NumericTreeNode` objects.
+        """
         # Returns a list of ancestory nodes nodes from this node to the root node
         path = []
         cnode = self
@@ -157,6 +222,11 @@ class NumericTreeNode:
         return path
     
     def get_trajectory(self):
+        """
+        Returns the trajectory (time and position) of the node and its ancestors.
+
+        :returns: A tuple :code:`(times, positions)` containing lists of times and positions.
+        """
         # Returns the time and position of each note
         cnode = self
         times = []
@@ -172,6 +242,12 @@ class NumericTreeNode:
         return times, positions
     
     def get_wf_func(self, func_init):
+        """
+        Returns a function that computes the wavefunction at this node by tracing back through its ancestry and applying the operation functions.
+
+        :param func_init: The function to compute the initial wavefunction.
+        :returns: A function that computes the wavefunction.
+        """
 
         # Get the ancestory nodes and the list of interferometer operations
         nodes = self.get_ancestry()
